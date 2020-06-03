db21259f0797ce0dca86ae0102c2c58363b3c740
Средство от прыщей влад <vlad.47304a@mail.ru>
2020-06-03 16:53:12 +0300

    add store service

--- Task/BusinessLogic/Services/StoreService.cs
+++ Task/BusinessLogic/Services/StoreService.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAcess.Entitites;
+using DataAcess.Interfaces;
+using DataAcess.Repositories;
 
 namespace BusinessLogic.Services
 {
-    class StoreService
+    public class StoreService : IDisposable
     {
+        private readonly IRepository<Store> _storeRepository;
+
+        private bool _disposed;
+
+        public StoreService(IRepository<Store> storeRepository)
+        {
+            _storeRepository = storeRepository;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _storeRepository.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
     }
 }
