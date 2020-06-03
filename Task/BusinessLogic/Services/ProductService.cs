90b0f88a0785efcda6af9e8a2623dc94a21cab10
Средство от прыщей влад <vlad.47304a@mail.ru>
2020-06-03 16:56:07 +0300

    add product service

---
+++ Task/BusinessLogic/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAcess.Entitites;
+using DataAcess.Interfaces;
+using DataAcess.Repositories;
+
+namespace BusinessLogic.Services
+{
+    public class ProductService : IDisposable
+    {
+        private readonly IRepository<Product> _productRepository;
+
+        private bool _disposed;
+
+        public ProductService(IRepository<Product> producrRepository)
+        {
+            _productRepository = producrRepository;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _productRepository.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+    }
+}
