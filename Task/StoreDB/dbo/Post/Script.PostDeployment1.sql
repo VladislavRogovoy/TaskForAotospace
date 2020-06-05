569247a21db424de75117675153ab060727229b3
Средство от прыщей влад <vlad.47304a@mail.ru>
2020-06-05 10:09:17 +0300

    change postdeploymentscript

--- Task/StoreDB/dbo/Post/Script.PostDeployment1.sql
+++ Task/StoreDB/dbo/Post/Script.PostDeployment1.sql
@@ -1,17 +1,17 @@
 ﻿--- Stores
 insert into dbo.Stores
-values ('Name 1', 'Address 1', 'Operating Mode 1'),
-('Name 2', 'Address 2', 'Operating Mode 2'),
-('Name 3', 'Address 3', 'Operating Mode 3')
+values ('Store 1', 'Address 1', 'Operating Mode 1'),
+('Store 2', 'Address 2', 'Operating Mode 2'),
+('Store 3', 'Address 3', 'Operating Mode 3')
 
 --- Products
 insert into dbo.Products
-values (1, 'Name 1', 'Description 1'),
-(1, 'Name 2', 'Description 2'),
-(1, 'Name 3', 'Description 3'),
-(2, 'Name 1', 'Description 1'),
-(2, 'Name 2', 'Description 2'),
-(2, 'Name 3', 'Description 3'),
-(3, 'Name 1', 'Description 1'),
-(3, 'Name 2', 'Description 2'),
-(3, 'Name 3', 'Description 3')
+values (1, 'Product 1', 'Description 1'),
+(1, 'Product 2', 'Description 2'),
+(1, 'Product 3', 'Description 3'),
+(2, 'Product 4', 'Description 4'),
+(2, 'Product 5', 'Description 5'),
+(2, 'Product 6', 'Description 6'),
+(3, 'Product 7', 'Description 7'),
+(3, 'Product 8', 'Description 8'),
+(3, 'Product 9', 'Description 9')
